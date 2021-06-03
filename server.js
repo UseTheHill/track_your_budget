d2260bffc8a78e6e0d2f166debc6ca9dcf3a84b5
Kayte McDonough <kaytemcdonough@gmail.com>
2021-06-03 18:33:52 -0400

    deploying

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budgetdb", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mighty-inlet-19941", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
